b83d375f4747a049e1517d9ac89ccf9c36dd25ea
ren <r999b66k222@outlook.jp>
2024-06-07 08:04:26 +0900

    こだわりと、メニューno

--- src/public/index.php
+++ src/public/index.php
@@ -48,19 +48,90 @@
   <main class="main">
 
     <!-- こだわり -->
-    <!-- グリッドを使いたいね・CSSはcliph-pathを使いたい -->
     <div class="detailed">
+
+      <h2 class="detailed-title">こだわり</h2>
+
+      <div class="detailed-wrapper">
+        <!-- 1 -->
+        <div class="detailed-soup">
+          <p class="detailed-soup__heading">スープ</p>
+          <p class="detailed-soup__text">無添加で、新鮮な野菜や厳選された鶏ガラなどを長時間煮込み、脂肪分を抑えたヘルシーなスープです。濃厚さを保ちながらも、胃に優しく、満足感のある一杯を提供します。</p>
+        </div>
+        <!-- 2 -->
+        <div class="detailed-noodles">
+          <p class="detailed-noodles__heading">自家製麺</p>
+          <p class="detailed-noodles__text">体に優しい全粒粉を使った自家製麺です。全粒粉ならではの豊かな風味とコシを楽しんでいただけます。化学調味料や保存料を一切使わず、健康志向の方にも安心してお召し上がりいただけます。</p>
+        </div>
+        <!-- 3 -->
+        <div class="detailed-topping">
+          <p class="detailed-topping__heading">トッピング</p>
+          <p class="detailed-topping__text">当店の食材はすべて無添加です。特にメンマは独自の製法で臭みを徹底的に取り除き、風味豊かに仕上げています。シャキシャキとした食感と、やさしい甘みがスープと絶妙にマッチします。</p>
+        </div>
+        <!-- 4 -->
+        <div class="detailed-comfortable">
+          <p class="detailed-comfortable__heading">快適空間</p>
+          <p class="detailed-comfortable__text">隣の席との間隔を広く取り、プライバシーを確保した半個室のようなカウンター席をご用意しています。これにより、リラックスしてお食事を楽しんでいただけます。</p>
+        </div>
+
+      </div>
 
     </div>
 
+    <div class="custom-hr"></div>
+
     <!-- メニュー -->
-    <!-- ヒロコードであった、HTML&CSSで開閉させるやつ -->
     <div class="menu">
+
+      <h2 class="menu-title">メニュー</h2>
+
+      <div class="menu-detail">
+
+        <details class="menu-detail__wrapper">
+          <summary class="menu-detail__wrapper-title">醤油ラーメン</summary>
+          <div class="menu-detail__wrapper-inner">
+            <div class="menu-detail__wrapper-imageWrapper">
+              
+            </div>
+            <p class="menu-detail__wrapper-text">使用している醤油は、長期熟成された天然醸造醤油で、深いコクと豊かな香りが特徴です。これにより、スープには独特の深みと風味が加わり、一口飲むごとに満足感が広がります。自家製麺とのコントラストをぜひご賞味ください。</p>
+          </div>
+        </details>
+
+        <details class="menu-detail__wrapper">
+          <summary class="menu-detail__wrapper-title">塩ラーメン</summary>
+          <div class="menu-detail__wrapper-inner">
+            <div class="menu-detail__wrapper-imageWrapper">
+              
+            </div>
+            <p class="menu-detail__wrapper-text">青ヶ島のひんぎゃの塩を使用しており、自然の恵みをたっぷりと感じられる一杯です。ひんぎゃの塩は、火山の噴気と海水が融合したミネラル豊富な塩です。一度味わえば忘れられない、この特別な塩ラーメンをぜひお試しください。</p>
+          </div>
+        </details>
+
+        <details class="menu-detail__wrapper">
+          <summary class="menu-detail__wrapper-title">メンマ</summary>
+          <div class="menu-detail__wrapper-inner">
+            <div class="menu-detail__wrapper-imageWrapper">
+              
+            </div>
+            <p class="menu-detail__wrapper-text">秘伝の調理法でじっくりと味付けされ、食感豊かで風味豊かな仕上がりになっています。一口食べれば、その独特の歯ごたえとコク深い味わいに魅了されることでしょう。</p>
+          </div>
+        </details>
+
+        <details class="menu-detail__wrapper">
+          <summary class="menu-detail__wrapper-title">チャーシュー丼</summary>
+          <div class="menu-detail__wrapper-inner">
+            <div class="menu-detail__wrapper-imageWrapper">
+              
+            </div>
+            <p class="menu-detail__wrapper-text">手間暇かけて煮込まれた柔らかいチャーシューは、自家製の特製タレが絡みつき、ご飯との相性抜群で、箸が止まらなくなること間違いありません。また、ごはんには玄米を使用し、食物繊維やミネラルが豊富で、満足感のある食事を提供します。</p>
+          </div>
+        </details>
+
+      </div>
 
     </div>
 
     <!-- アクセス・営業時間 -->
-    <!-- アクセスと営業時間はまとめて、横並びでいいのでは、レスポンシブで縦並びに -->
     <div class="location">
 
       <!-- アクセス -->
